9d1e6a1f70ce3d6976b0d59d5087ff6ee32f0569
JeranDev <jeranjb@gmail.com>
2021-02-03 12:02:05 -0700

    prepare for Heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,32 @@
-import routes from './routes.js'
+import path from 'path'
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+//Routes
+import routes from './routes.js'
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
 
-const port = process.env.PORT || 5000
-
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 app.use('/api/plants', routes)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
+
+const port = process.env.PORT || 5000
 
 app.listen(
   port,
